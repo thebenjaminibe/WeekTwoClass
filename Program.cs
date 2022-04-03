ed1cf71e3c3a4c6011631f55a92a2aa403ce0439
thebenjaminibe <frankynoble@gmail.com>
2022-04-03 15:26:23 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WeekTwoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myName = "Benjamin";
+            var myAge = 30;
+            var nairaToDollars = 570.29;
+            var anonTypes = new {
+                name = "anonymous",
+                project = "Dotnet",
+                version = 5.0
+
+            };
+
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(anonTypes.name);
+            Console.WriteLine(anonTypes.project);
+            Console.WriteLine(anonTypes.version);
+        }
+    }
+}
