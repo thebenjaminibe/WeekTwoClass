4158a40f9b50df787a52a4e9f5adeba915af8a61
thebenjaminibe <frankynoble@gmail.com>
2022-04-03 17:57:09 +0100

    lab 2

--- Program.cs
+++ Program.cs
@@ -6,23 +6,24 @@ namespace WeekTwoClass
     {
         static void Main(string[] args)
         {
-            var myName = "Benjamin";
-            var myAge = 30;
-            var nairaToDollars = 570.29;
-            var anonTypes = new {
-                name = "anonymous",
-                project = "Dotnet",
-                version = 5.0
+            string myName = "Benjamin";
+            int myAge = 30;
+            double nairaToDollars = 570.29;
+            char myLetter = 'D';
+            bool myBool = true;
+            long myNum = 150000000000000L;
+            byte b1 = 255;
+            sbyte sb2 = -128;
 
-            };
+            Console.WriteLine(myName);
+            Console.WriteLine(myAge);
+            Console.WriteLine(nairaToDollars);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myNum);
+            Console.WriteLine(b1);
+            Console.WriteLine(sb2);
 
-
-
-
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(anonTypes.name);
-            Console.WriteLine(anonTypes.project);
-            Console.WriteLine(anonTypes.version);
         }
     }
 }
